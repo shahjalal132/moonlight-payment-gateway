08a832cb94870a71c4b3ca1b7133c3f362059a5a
shahjalal <rjshahjalal132@gmail.com>
2025-01-19 10:07:48 +0600

    added gateway-nmi-konnektive-crm.php file

--- loader.php
+++ loader.php
@@ -4,4 +4,5 @@
 include_once PLUGIN_BASE_PATH . '/inc/helpers/helper-functions.php';
 
 // require payment gateway file
-require_once PLUGIN_BASE_PATH . '/inc/gateways/gateway-moonlight.php';
+// require_once PLUGIN_BASE_PATH . '/inc/gateways/gateway-moonlight.php'; // moonlight gateway
+require_once PLUGIN_BASE_PATH . '/inc/gateways/gateway-nmi-konnektive-crm.php'; // nmi and konnektive crm integration
